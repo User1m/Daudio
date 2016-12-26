2e18caeba8652a7809deda8e99f8c9a858dfbc17
Claudius Mbemba <mbembac@gmail.com>
2016-12-26 06:34:31 -0500

    updated pods - added objection

---
+++ Pods/Target Support Files/Objection/Objection-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "JSObjectFactory.h"
+#import "JSObjection.h"
+#import "JSObjectionBindingEntry.h"
+#import "JSObjectionEntry.h"
+#import "JSObjectionInjector.h"
+#import "JSObjectionInjectorEntry.h"
+#import "JSObjectionModule.h"
+#import "JSObjectionProviderEntry.h"
+#import "JSObjectionRuntimePropertyReflector.h"
+#import "JSObjectionUtils.h"
+#import "NSObject+Objection.h"
+#import "Objection.h"
+
+FOUNDATION_EXPORT double ObjectionVersionNumber;
+FOUNDATION_EXPORT const unsigned char ObjectionVersionString[];
+
