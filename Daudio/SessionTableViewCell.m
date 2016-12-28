49d400969b732a447664eb6427f20c33453e4eb8
Claudius Mbemba <mbembac@gmail.com>
2016-12-28 16:55:04 -0500

    fixed views hidding on tablecell selcted state

--- Daudio/SessionTableViewCell.m
+++ Daudio/SessionTableViewCell.m
@@ -8,17 +8,32 @@
 
 #import "SessionTableViewCell.h"
 
+@interface SessionTableViewCell()
+
+@property (weak, nonatomic) IBOutlet UIView *bar1;
+@property (weak, nonatomic) IBOutlet UIView *bar2;
+
+@end
+
 @implementation SessionTableViewCell
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
-}
-
+//https://stackoverflow.com/questions/6745919/uitableviewcell-subview-disappears-when-cell-is-selected
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
+    
+    if (selected){
+        self.bar1.backgroundColor = [UIColor darkGrayColor];
+        self.bar2.backgroundColor = [UIColor darkGrayColor];
+    }
+}
 
-    // Configure the view for the selected state
+- (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated{
+    [super setHighlighted:highlighted animated:animated];
+    
+    if (highlighted){
+        self.bar1.backgroundColor = [UIColor darkGrayColor];
+        self.bar2.backgroundColor = [UIColor darkGrayColor];
+    }
 }
 
 @end
